1657d156dd79c9d4da47c172a734365f5a9253fd
apple <apple@abcd.local>
2022-11-29 11:13:52 +0500

    Vue Project initialized

---
+++ src/components/Header.vue
@@ -0,0 +1,50 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div>
+    <!-- As a link -->
+    <nav class="navbar navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+      </div>
+    </nav>
+
+    <!-- As a heading -->
+    <nav class="navbar navbar-light bg-light">
+      <div class="container-fluid">
+        <span class="navbar-brand mb-0 h1">Navbar</span>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: center;
+  }
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,66 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="mt-4">
+    <div class="row">
+      <div class="col-md-6">
+        <div class="contianerElectric">
+          <div class="paddingInside">
+            <div class="title">KINGSONG S18</div>
+            <div class="description mt-2">
+              Electric Unicycle with<br />
+              air damping suspension
+            </div>
+            <div class="priceContainer">
+              <typography class="delPrice">$1399.00</typography><br />
+              <typography class="price">$1099.00</typography>
+            </div>
+            <div class="mt-2">
+              <button type="button" class="btn btnBuy">Buy Now</button>
+            </div>
+          </div>
+        </div>
+
+        <div class="contianerFloadable mt-1">
+          <div class="paddingInside">
+            <div class="title">KINGSONG S18</div>
+            <div class="description mt-2">
+              Foldable electric scooter<br />
+              250W motor waterproof
+            </div>
+            <div class="floadbalePriceContainer">
+              <typography class="price">$1099.00</typography>
+            </div>
+            <div class="mt-2">
+              <button type="button" class="btn btnBuy">Buy Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 contianerCycle">
+        <div class="">
+          <div class="paddingInside">
+            <div class="title">KINGSONG S18</div>
+            <div class="description mt-2">
+              16-Inch electric unicycle<br />
+              with mobile app
+            </div>
+            <div class="floadbalePriceContainer">
+              <typography class="price">$1099.00</typography>
+            </div>
+            <div class="mt-2">
+              <button type="button" class="btn btnBuy">Buy Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
